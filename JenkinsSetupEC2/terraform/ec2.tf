7c89255b647cd9048aa90224c42164137d5f98f1
nileshgk <nileshgk774@gmail.com>
2026-08-04 04:43:56 +0530

    ec2 fmt

--- JenkinsSetupEC2/terraform/ec2.tf
+++ JenkinsSetupEC2/terraform/ec2.tf
@@ -1,15 +1,17 @@
-# Create SSH key pair
+# Generate SSH Key Pair
 resource "tls_private_key" "jenkins" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
+# Save private key locally
 resource "local_file" "private_key" {
   filename        = "${path.module}/../keys/jenkins.pem"
   content         = tls_private_key.jenkins.private_key_pem
   file_permission = "0400"
 }
 
+# Create AWS Key Pair
 resource "aws_key_pair" "jenkins" {
   key_name   = "jenkins-key"
   public_key = tls_private_key.jenkins.public_key_openssh
@@ -17,12 +19,9 @@ resource "aws_key_pair" "jenkins" {
 
 # Jenkins EC2 Instance
 resource "aws_instance" "jenkins" {
-
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  subnet_id = aws_subnet.public.id
-
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public.id
   associate_public_ip_address = true
 
   key_name = aws_key_pair.jenkins.key_name
@@ -32,41 +31,28 @@ resource "aws_instance" "jenkins" {
   ]
 
   iam_instance_profile = aws_iam_instance_profile.jenkins_profile.name
-  
+
   user_data = file("${path.module}/../scripts/install_jenkins.sh")
 
-
   root_block_device {
-
-    volume_size = 30
-
-    volume_type = "gp3"
-
-    encrypted = true
-
+    volume_size           = 30
+    volume_type           = "gp3"
+    encrypted             = true
     delete_on_termination = true
-
   }
 
   metadata_options {
-
     http_endpoint = "enabled"
-
-    http_tokens = "required"
-
+    http_tokens   = "required"
   }
 
   tags = {
-
-    Name = "${var.project_name}-jenkins"
-
+    Name        = "${var.project_name}-jenkins"
     Environment = "Dev"
-
-    ManagedBy = "Terraform"
-
+    ManagedBy   = "Terraform"
   }
-
 }
+
 #########################################
 # Attach AdministratorAccess
 #########################################
